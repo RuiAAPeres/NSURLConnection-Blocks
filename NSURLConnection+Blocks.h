5fbc71b076864e368163ef413a4324d62442d89e
R. Peres <sauron.is.alive@gmail.com>
2012-07-11 14:35:27 +0200

    v1.0

---
+++ NSURLConnection+Blocks.h
@@ -0,0 +1,26 @@
+//
+//  NSURLRequest+NSURLRequest_Blocks.h
+//  Black Magic
+//
+//  Created by Rui Peres on 7/11/12.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^CompletionBlock)(NSData *data);
+typedef void(^FailBlock)(NSError *error);
+typedef void(^CleanBlock)();
+
+@interface NSURLConnection (NSURLConnection_Blocks)
+
+/**
+ It will receive a request, a onCompletion block and a onFail block. One of them will be executed
+ according to the request's response
+ @param request a NSURLRequest
+ @param completionBlock Block executed on success
+ @param failBlock executed when the connection failed
+ @return NSURLConnection* a pointer a NSURLConnection object
+ */
++(NSURLConnection*)connectionWithRequest:(NSURLRequest*)request onCompletion:(CompletionBlock)completionBlock onFail:(FailBlock)failBlock;
+
+@end
---
+++ NSURLConnection+Blocks.m
@@ -0,0 +1,63 @@
+//
+//  NSURLRequest+NSURLRequest_Blocks.m
+//  Black Magic
+//
+//  Created by Rui Peres on 7/11/12.
+//
+
+#import "NSURLConnection+Blocks.h"
+
+@implementation NSURLConnection (NSURLConnection_Blocks)
+
+static CompletionBlock _completionBlock;
+static FailBlock _failBlock;
+static CleanBlock _cleanBlock;
+static NSMutableData *webData;
+
+#pragma mark - Public Methods
+
++(NSURLConnection*)connectionWithRequest:(NSURLRequest*)request onCompletion:(CompletionBlock)completionBlock onFail:(FailBlock)failBlock
+{
+    _cleanBlock = [^{
+        [_failBlock autorelease];
+        [_completionBlock autorelease];
+        [_cleanBlock autorelease];
+        [webData autorelease];
+    } copy];
+    
+    [_completionBlock release];
+    [_failBlock release];
+    
+    _completionBlock = [completionBlock copy];
+    _failBlock = [failBlock copy];
+    
+    NSURLConnection *connection = [NSURLConnection connectionWithRequest:request delegate:[self class]];
+    
+    return connection;
+}
+
+#pragma mark - NSURLConnectionDelegate Implementation
+
++(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    _failBlock(error);
+    _cleanBlock();
+}
+
++(void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    _completionBlock([NSData dataWithData:webData]);
+    _cleanBlock();
+}
+
++(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{    
+    webData = [[NSMutableData dataWithLength:1024] retain];
+	[webData setLength: 0];
+}
+
++(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
+	[webData appendData:data];
+}
+
+@end
